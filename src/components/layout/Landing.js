05adf56eff48e84b80701db97ee14bda31597e6b
Vince Oblak <voblak2@gmail.com>
2020-01-11 21:34:04 -0600

    Added two png icons and modified Landing.js

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+/* Below originally had: <section class>, but dev tools flagged it asking for: <section className> instead so I changed it and the error cleared.*/
 const Landing = () => {
-    return (
-        <section class="landing">
-            <h2>Her children arise up, and call her blessed; her husband also, and he praiseth her. </h2>
-            <p>Proverbs 31:29 KJV</p>
-        </section>
-    )
-}
+  return (
+    <section className='landing'>
+      <h2>
+        Her children arise up, and call her blessed; her husband also, and he
+        praiseth her.{' '}
+      </h2>
+      <p>Proverbs 31:28 KJV</p>
+    </section>
+  );
+};
 
-export default Landing
+export default Landing;
