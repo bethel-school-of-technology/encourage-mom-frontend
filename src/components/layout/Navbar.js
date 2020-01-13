9bbde2a06e1bd8a498876200d482dd113bfc38d8
Vince Oblak <voblak2@gmail.com>
2020-01-13 09:09:59 -0600

    Some modifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,30 +4,40 @@ import profile from '../img/profile.png'
 
 const Navbar = () => {
     return (
-        <header>
-                <nav>
-                    <ul>
-                        <li><h1>Crowned Jewels</h1></li>
-                            <li className="dropdown">
-                                <div className="dropbtn">
-                                    <Link to = "/profile">
-                                        <img src={profile}  width="45px" />
-                                    </Link>
-                                    <div className="dropdown-content" >
-                                        <Link to = "/signup">Sign-up</Link>
-                                        <Link to = "/login">Login</Link>
-                                        <Link to = "/logout">Logout</Link>
-                                    </div>
-                                </div>
-                            </li>
-                        <li className="nav-right"><Link to ="/contact">Contact</Link></li>
-                        <li className="nav-right"><Link to = "/posts">Posts</Link></li>
-                        <li className="nav-right"><Link to = "/encouragement">Weekly Encouragment</Link> </li>
-                        <li className="nav-right"><Link to ="/home">Home</Link></li>
-                    </ul>
-                </nav>
-        </header>
-    )
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <h1>Crowned Jewels</h1>
+            </li>
+            <li className='dropdown'>
+              <div className='dropbtn'>
+                <Link to='/profile'>
+                  <img src={profile} width='45px' />
+                </Link>
+                <div className='dropdown-content'>
+                  <Link to='/signup'>Sign-up</Link>
+                  <Link to='/login'>Login</Link>
+                  <Link to='/logout'>Logout</Link>
+                </div>
+              </div>
+            </li>
+            <li className='nav-right'>
+              <Link to='/contact'>Contact</Link>
+            </li>
+            <li className='nav-right'>
+              <Link to='/posts'>Posts</Link>
+            </li>
+            <li className='nav-right'>
+              <Link to='/encouragement'>Weekly Encouragment</Link>{' '}
+            </li>
+            <li className='nav-right'>
+              <Link to='/home'>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
 }
 
 export default Navbar
