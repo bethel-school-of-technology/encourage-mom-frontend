8073025775494f5504e0f139cef5f181e38de1c7
Vince Oblak <voblak2@gmail.com>
2020-01-13 20:07:58 -0600

    Corrected spelling error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Landing from './components/layout/Landing';
 import Signup from './components/pages/Signup';
 import Login from './components/pages/Login';
 import home from './components/pages/home';
-import encouragment from './components/pages/weekly-encouragement';
+import encouragement from './components/pages/weekly-encouragement';
 import posts from './components/pages/posts';
 import contact from './components/pages/contact';
 
@@ -18,30 +18,28 @@ import store from './store';
 import './App.css';
 
 const App = () => (
-
-<Provider store={store}>
-
-<Router>
-  <Fragment>
-      <Navbar/>
-      <Landing />
-      <section className="container1">
-      <Route exact path="/home" component={home} />
-      <Route exact path="/encouragement" component={encouragment} />
-      <Route exact path="/posts" component={posts} />
-      <Route exact path="/contact" component={contact} />
-      </section>
-      <section className="container2">
-        <Switch>
-          <Route exact path ="/signup" component={Signup} />
-          <Route exact path ="/login" component={Login} />
-          {/* <Route exact path="/logout" component={Logout} /> */}
-        </Switch>
-      </section>
-  </Fragment>
-</Router>
-</Provider>
-) ;
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Landing />
+        <section className='container1'>
+          <Route exact path='/home' component={home} />
+          <Route exact path='/encouragement' component={encouragement} />
+          <Route exact path='/posts' component={posts} />
+          <Route exact path='/contact' component={contact} />
+        </section>
+        <section className='container2'>
+          <Switch>
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/login' component={Login} />
+            {/* <Route exact path="/logout" component={Logout} /> */}
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+); ;
 
 
 
