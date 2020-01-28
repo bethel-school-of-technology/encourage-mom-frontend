280d87b4b8b663e52e087169cdc3154825c58dab
unknown <bump.jason@gmail.com>
2020-01-28 10:45:22 -0600

    heroku and code for weekly encouragement that is commented out for now

--- src/components/pages/weekly-encouragement.js
+++ src/components/pages/weekly-encouragement.js
@@ -10,74 +10,74 @@
 
 // export default Encouragement
 
-import React, { Component } from "react";
-class Encouragement extends Component {
-  render() {
-    return (
-      <form>
-        <fieldset>
-          <input
-            placeholder="test"
-            className="email-signup--input"
-            ref="email"
-            value={this.state.email}
-          />
-          <button onClick={this.success} className="email-signup--submit">
-            Submit
-          </button>
-        </fieldset>
-      </form>
-    );
-  }
-}
+// import React, { Component } from "react";
+// class Encouragement extends Component {
+//   render() {
+//     return (
+//       <form>
+//         <fieldset>
+//           <input
+//             placeholder="test"
+//             className="email-signup--input"
+//             ref="email"
+//             value={this.state.email}
+//           />
+//           <button onClick={this.success} className="email-signup--submit">
+//             Submit
+//           </button>
+//         </fieldset>
+//       </form>
+//     );
+//   }
+// }
 
-class EmailSignup extends React.Component {
-  getInitialState() {
-    return {
-      email: "hi"
-    };
-  }
+// class EmailSignup extends React.Component {
+//   getInitialState() {
+//     return {
+//       email: "hi"
+//     };
+//   }
 
-  emailInput(event) {
-    this.setState({
-      email: event.target.value
-    });
-  }
+//   emailInput(event) {
+//     this.setState({
+//       email: event.target.value
+//     });
+//   }
 
-  proceed(e) {
-    e.preventDefault();
-  }
+//   proceed(e) {
+//     e.preventDefault();
+//   }
 
-  validateEmail(event) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(event);
-  }
+// //   validateEmail(event) {
+// //     // var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// //     return re.test(event);
+// //   }
 
-  render() {
-    return (
-      <section className="email-signup">
-        <div className="email-signup__modal">
-          <form>
-            <fieldset>
-              <input
-                placeholder="test"
-                className="email-signup--input"
-                ref="email"
-                value={this.state.email}
-                onChange={this.emailInput}
-                error="Hey, your email is invalid!"
-                empty="Email can't be empty"
-                showError={this.state.showError}
-              />
-              <button onClick={this.success} className="email-signup--submit">
-                Submit
-              </button>
-            </fieldset>
-          </form>
-        </div>
-      </section>
-    );
-  }
-}
-export default Encouragement;
-React.render(<EmailSignup />, document.getElementById("email"));
+//   render() {
+//     return (
+//       <section className="email-signup">
+//         <div className="email-signup__modal">
+//           <form>
+//             <fieldset>
+//               <input
+//                 placeholder="test"
+//                 className="email-signup--input"
+//                 ref="email"
+//                 value={this.state.email}
+//                 onChange={this.emailInput}
+//                 error="Hey, your email is invalid!"
+//                 empty="Email can't be empty"
+//                 showError={this.state.showError}
+//               />
+//               <button onClick={this.success} className="email-signup--submit">
+//                 Submit
+//               </button>
+//             </fieldset>
+//           </form>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+// export default Encouragement;
+// React.render(<EmailSignup />, document.getElementById("email"));
