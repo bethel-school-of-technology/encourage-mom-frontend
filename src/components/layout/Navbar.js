1131f692b44cb5da7ab393486d2c21229b95ed0c
Vince Oblak <voblak2@gmail.com>
2020-01-27 20:46:12 -0600

    Added our logoand file to hold it.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,43 +1,43 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import profile from '../img/profile.png'
+import profile from '../img/profile.png';
 
 const Navbar = () => {
-    return (
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <h1>Crowned Jewels</h1>
-            </li>
-            <li className='dropdown'>
-              <div className='dropbtn'>
-                <Link to='/profile'>
-                  <img src={profile} width='45px' />
-                </Link>
-                <div className='dropdown-content'>
-                  <Link to='/signup'>Sign-up</Link>
-                  <Link to='/login'>Login</Link>
-                  <Link to='/logout'>Logout</Link>
-                </div>
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <h1>Crowned Jewels</h1>
+          </li>
+          <li className='dropdown'>
+            <div className='dropbtn'>
+              <Link to='/profile'>
+                <img src={profile} width='45px' />
+              </Link>
+              <div className='dropdown-content'>
+                <Link to='/signup'>Sign-up</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/logout'>Logout</Link>
               </div>
-            </li>
-            <li className='nav-right'>
-              <Link to='/contact'>Contact</Link>
-            </li>
-            <li className='nav-right'>
-              <Link to='/posts'>Posts</Link>
-            </li>
-            <li className='nav-right'>
-              <Link to='/encouragement'>Weekly Encouragement</Link>{' '}
-            </li>
-            <li className='nav-right'>
-              <Link to='/home'>Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-}
+            </div>
+          </li>
+          <li className='nav-right'>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li className='nav-right'>
+            <Link to='/posts'>Posts</Link>
+          </li>
+          <li className='nav-right'>
+            <Link to='/encouragement'>Weekly Encouragement</Link>{' '}
+          </li>
+          <li className='nav-right'>
+            <Link to='/home'>Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
