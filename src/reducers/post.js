da604151d22aa724786564e704dc3be16bda86bc
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-01-29 17:02:49 -0800

    added post.js file in reducer file

--- src/reducers/post.js
+++ src/reducers/post.js
@@ -16,8 +16,43 @@ const initalState = {
     error: {}
 };
 
-// export default function(state = initalState, action) {
-//     const {type, payload} = action;
+export default function(state = initalState, action) {
+    const {type, payload} = action;
 
-//     switch 
-// }
+    switch(type) {
+        case GET_POSTS:
+            return{
+                ...state,
+                posts: payload,
+                loading: false
+            };
+        case GET_POST:
+            return{
+                ...state,
+                posts: payload,
+                loading: false
+            };
+        case CREATE_POST:
+            return {
+                ...state,
+                posts: [payload, ...state.posts],
+                loading: false
+            };
+        case DELETE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== payload),
+                loading: false
+            };
+        case POST_ERROR:
+            return {
+                ...state,
+                error: payload,
+                loading: false
+            }
+        default:
+            return state;
+
+        
+    } 
+}
