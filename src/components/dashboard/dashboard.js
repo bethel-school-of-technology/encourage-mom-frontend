65eb938022596c83189bd1a1911105decb1cd8a8
Vince Oblak <voblak2@gmail.com>
2020-02-01 00:59:43 -0600

    Minor changes

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,13 +3,12 @@ import React, { Component } from "react";
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-            <h1>Welcome 
-                {/* { user.username}  */}
-                </h1>
-            
-            </div>
-
+          <div>
+            <h1>
+              Welcome
+              {/* { user.username}  */}
+            </h1>
+          </div>
         );
     }
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return (
-                <div>
-                <h1>Home</h1>
-                </div>
-            
+          <div>
+            <h1>Home</h1>
+            <h3>CHecking to see if it is erroring out</h3>
+          </div> 
         );
     }
 }
