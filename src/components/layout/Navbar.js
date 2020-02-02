27311f5dc6b033616615c5707f64598789a9a1d3
Vince Oblak <voblak2@gmail.com>
2020-02-01 23:41:02 -0600

    Trying to get code rinning

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,8 +5,41 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 import menu from '../img/menu.png';
-
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const guestLinks = (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <h1>Crowned Jewels</h1>
+          </li>
+          <li className='dropdown'>
+            <div className='dropbtn'>
+              <Link to='#'>
+                <img alt='menu' src={menu} width='45px' padding='50px' />
+              </Link>
+              <div className='dropdown-content'>
+                <Link to='/signup'>Sign Up</Link>
+                <Link to='/login'>Login</Link>
+              </div>
+            </div>
+          </li>
+          <li className='nav-right'>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li className='nav-right'>
+            <Link to='/posts'>Posts</Link>
+          </li>
+          <li className='nav-right'>
+            <Link to='/encouragement'>Encouragement</Link>{' '}
+          </li>
+          <li className='nav-right'>
+            <Link to='/'>Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
   const authLinks = (
     <header>
       <nav>
@@ -22,7 +55,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
               <div className='dropdown-content'>
                 <Link to='/create-post'>Create Post</Link>
                 <Link to='/profiles'>User Bios</Link>
-                {/* <a onClick={logout} href="/#">Logout</a>  */}
                 <Link to='/login' onClick={logout}>
                   Logout
                 </Link>
@@ -35,69 +67,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <li className='nav-right'>
             <Link to='/posts'>Posts</Link>
           </li>
-
           <li className='nav-right'>
             <Link to='/encouragement'>Encouragement</Link>{' '}
           </li>
-
-const Navbar = ({ auth: {isAuthenticated, loading }, logout }) => {
-    const guestLinks = (
-        <header>
-                <nav>
-                    <ul>
-                    <li><h1>Crowned Jewels</h1></li>
-                    <li className="dropdown">
-                                <div className="dropbtn">
-                                    {/* <Link to="#">
-                                        <img alt='menu' src={menu}  width="35px" padding='50px'/>
-                                    </Link> */}
-                                <div className="drop-btn">
-                                   <Link to = "/profile">
-                                         <img alt='menu' src={menu}  width="45px" />
-                                    </Link>
-                                    <div className="dropdown-content" >
-                                        <Link to = "/signup">Sign Up</Link>
-                                        <Link to = "/login">Login</Link> 
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="nav-right"><Link to ="/contact">Contact</Link></li>
-                        <li className="nav-right"><Link to = "/posts">Posts</Link></li>
-                        <li className="nav-right"><Link to = "/encouragement">Encouragement</Link> </li>
-                        <li className="nav-right"><Link to ="/">Home</Link></li>
-                    </ul>
-                </nav>
-        </header>
-    );
-    const authLinks = (
-        <header>
-        <nav>
-            <ul>
-                <li><h1>Crowned Jewels</h1></li>
-                <li className="dropdown">
-                        <div className="drop-btn">
-                            <Link to = "/profile">
-                                <img alt='profile' src={profile}  width="45px" />
-                            </Link>
-                            <div className="dropdown-content" >
-                                <Link to = "/create-post">Create Post</Link>
-                                <Link to = "/profiles">User Bios</Link>                               
-                                {/* <a onClick={logout} href="/#">Logout</a>  */}
-                                <Link to ="/login" onClick={logout}>Logout</Link>
-                            </div>
-                        
-                        </div>
-                    </li>
-                <li className="nav-right"><Link to ="/contact">Contact</Link></li>
-                <li className="nav-right"><Link to = "/posts">Posts</Link></li>
-                <li className="nav-right"><Link to = "/encouragement">Encouragement</Link> </li>
-                <li className="nav-right"><Link to ="/">Home</Link></li>
-            </ul>
-        </nav>
-</header>
-    )
-
+          <li className='nav-right'>
+            <Link to='/'>Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
   return (
     <header>
       {/* <h1>Crowned Jewels</h1> */}
@@ -109,16 +88,13 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout }) => {
     </header>
   );
 };
-
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
   // isAdmin: PropTypes.object.isRequired
 };
-
 const mapStateToProps = state => ({
   auth: state.auth
   // isAdmin: state.isAdmin
 });
-
 export default connect(mapStateToProps, { logout })(Navbar);
