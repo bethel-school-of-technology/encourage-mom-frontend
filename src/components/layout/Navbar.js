8c2ea84908faa18e575077336fbeab0a0977fb31
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-02-01 21:42:54 -0800

    Trying to get code updated and running

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -17,19 +17,14 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout }) => {
                     <li><h1>Crowned Jewels</h1></li>
                     <li className="dropdown">
                                 <div className="dropbtn">
-                                    {/* <Link to="#">
-                                        <img alt='menu' src={menu}  width="35px" padding='50px'/>
-                                    </Link> */}
-                                <div className="drop-btn">
-                                   <Link to = "/profile">
-                                         <img alt='menu' src={menu}  width="45px" />
+                                    <Link to="#">
+                                        <img alt='menu' src={menu}  width="45px" padding='50px'/>
                                     </Link>
                                     <div className="dropdown-content" >
                                         <Link to = "/signup">Sign Up</Link>
                                         <Link to = "/login">Login</Link> 
                                     </div>
                                 </div>
-                            </div>
                         </li>
                         <li className="nav-right"><Link to ="/contact">Contact</Link></li>
                         <li className="nav-right"><Link to = "/posts">Posts</Link></li>
@@ -52,7 +47,6 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout }) => {
                             <div className="dropdown-content" >
                                 <Link to = "/create-post">Create Post</Link>
                                 <Link to = "/profiles">User Bios</Link>                               
-                                {/* <a onClick={logout} href="/#">Logout</a>  */}
                                 <Link to ="/login" onClick={logout}>Logout</Link>
                             </div>
                         
